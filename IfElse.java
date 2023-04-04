c203be20f864261bfc3b4f2e8db2d562aef2e87d
brainmentorspvtltd <kajal1231995@gmail.com>
2023-04-04 13:07:57 +0530

    java basics

---
+++ IfElse.java
@@ -0,0 +1,79 @@
+import java.beans.Expression;
+
+public class IfElse {
+    public static void main(String[] args) {
+        
+
+    //block => { } is a part of code which is enclosed with brackets
+     boolean  bool= true;
+     int a=80;
+     int b=80;
+     int c=80;
+
+
+     if(a>b && a>c){
+        System.out.println("a is the greatest element");
+     }
+     else if(b>a && b>c){
+        System.out.println("b is the greatest element");
+     }
+     else if(c>a && c>b){
+        System.out.println("c is the greatest element");
+     }
+     else{
+        System.out.println("all are equal");
+     }
+
+// ==, !=, < ,>, <=,>= 
+// || && !
+
+
+
+    //  if(a==b){
+        
+    //     System.out.print("if part");
+    //  }
+    //  else{
+    //     System.out.println("else part");
+    //  }
+       
+    //  if(a!=b){
+        
+    //     System.out.print("if part");
+    //  }
+    //  else{
+    //     System.out.println("else part");
+    //  }
+
+    //  if(a<b){
+        
+    //     System.out.print("if part");
+    //  }
+    //  else{
+    //     System.out.println("else part");
+    //  }
+    //  if(a>b){
+        
+    //     System.out.print("if part");
+    //  }
+    //  else{
+    //     System.out.println("else part");
+    //  }
+    //  if(a<=b){
+        
+    //     System.out.print("if part");
+    //  }
+    //  else{
+    //     System.out.println("else part");
+    //  }
+    //  if(a>=b){
+        
+    //     System.out.print("if part");
+    //  }
+    //  else{
+    //     System.out.println("else part");
+    //  }
+
+
+    }
+}
---
+++ Test.java
@@ -0,0 +1,25 @@
+class Test{
+    public static void main(String[] args) {
+        
+       System.out.print("Hello World");
+       System.out.println("Hello World");
+       System.out.print("hello world 3");
+        
+        // byte byteNum =Byte.MAX_VALUE;
+        // short shortNum =-32768;
+        // int intNum = 2147483647;
+        // long longNum= Long.MAX_VALUE;
+        // System.out.println(byteNum+" =>  next num =  "+shortNum+" =>  next num = "+intNum+" =>  next num =  "+longNum);
+        // System.out.println(byteNum+" "+shortNum+" "+intNum+" "+longNum);
+        // System.out.println(byteNum+"\n"+shortNum+"\n"+intNum+"\n"+longNum);
+        // System.out.println(byteNum);
+        // System.out.println(shortNum);
+        // System.out.println(intNum);
+        // System.out.println(longNum);
+
+
+    }
+}
+
+
+
