0990897278141afd56685bf61333a77cb7226618
brainmentorspvtltd <kajal1231995@gmail.com>
2023-04-04 14:25:00 +0530

    ternary operator ans if else

--- nestedIfElse.java
+++ nestedIfElse.java
@@ -1,71 +1,102 @@
+import java.beans.Expression;
+
 public class nestedIfElse {
     public static void main(String[] args) {
-        System.out.println("hello world");
+        // System.out.println("hello world");
 
-        boolean raining=false;
+        int a=90;
+        int b=70;
+
+        int result = a>b?a+b:a-b;
+        ?:
+        // int result = Expression1 ? Expression2 : Expression3;
+        System.out.println(result);
+
+        boolean raining=true;
         boolean umbrella =false;
         boolean raincoat =false;
         boolean breakfast =false;
         boolean lunchbox = true;
 
+        //teranry
+        
+        // String str=  raining==true  ?"it is raining ":"it not raining";
+        // System.out.println(str);
+
+        // if(raining==true && (umbrella ==true|| raincoat==true)){
+
+        //     if(breakfast==true || lunchbox ==true){
+        //         System.out.println("you can go to school");
+        //     }
+        //     else{
+        //         System.out.println("you cannot go to school");
+        //     }
+
+        // }
+        // else{
+        //     System.out.println("you cannot go to school");
+        // }
 
 
-        if(raining==true){
-            if(umbrella==true){
-                if(breakfast==true){
-                    System.out.println("go to school");
-                }
-                else{
-                    if(lunchbox==true){
-                        System.out.println("go to school");
-                    }
-                    else{
-                        System.out.println("dont go to school");
-                    }
-                }
-            }
-            else{
-                if(raincoat==true){
-                    if(breakfast==true){
-                        System.out.println("go to school");
-                    }
-                    else{
-                        if(lunchbox==true){
-                            System.out.println("go to school");
-                        }
-                        else{
-                            System.out.println("dont go to school");
-                        }
-                    }
-                }
-                else{
-                    System.out.println("dont go to school");
-                }
-            }
 
-        }
-        else{
-            if(breakfast==true){
-                System.out.println("go to school");
-            }
-            else{
-                if(lunchbox==true){
-                    System.out.println("go to school");
-                }
-                else{
-                    System.out.println("dont go to school");
-                }
-            }
-        }
+
+ 
+        // if(raining==true ){
+        //     if(umbrella==true){
+        //         if(breakfast==true){
+        //             System.out.println("go to school");
+        //         }
+        //         else{
+        //             if(lunchbox==true){
+        //                 System.out.println("go to school");
+        //             }
+        //             else{
+        //                 System.out.println("dont go to school");
+        //             }
+        //         }
+        //     }
+        //     else{
+        //         if(raincoat==true){
+        //             if(breakfast==true){
+        //                 System.out.println("go to school");
+        //             }
+        //             else{
+        //                 if(lunchbox==true){
+        //                     System.out.println("go to school");
+        //                 }
+        //                 else{
+        //                     System.out.println("dont go to school");
+        //                 }
+        //             }
+        //         }
+        //         else{
+        //             System.out.println("dont go to school");
+        //         }
+        //     }
+
+        // }
+        // else{
+        //     if(breakfast==true){
+        //         System.out.println("go to school");
+        //     }
+        //     else{
+        //         if(lunchbox==true){
+        //             System.out.println("go to school");
+        //         }
+        //         else{
+        //             System.out.println("dont go to school");
+        //         }
+        //     }
+        // }
 
 
        
 
-        int a=90;//initialization
-        int b,c;//multiple declaratiomn
-        int d=90, e=89;//multi initial
-        b=78;
-        c=67;
+        // int a=90;//initialization
+        // int b,c;//multiple declaratiomn
+        // int d=90, e=89;//multi initial
+        // b=78;
+        // c=67;
 
         // if( a <b){
         //     if(b<c){
