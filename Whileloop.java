66f317361eaec715a79f58166d8fbf377307c2ce
brainmentorspvtltd <kajal1231995@gmail.com>
2023-04-06 14:14:59 +0530

    while and break statement

--- Whileloop.java
+++ Whileloop.java
@@ -1,14 +1,27 @@
 public class Whileloop {
     public static void main(String[] args) {
         
+        int i =1;
+        while(true){
+            System.out.println(i*13+" " +i);
+            if(i>90){
+                break;
+            }
+            else{
+                i=i*13;
+            }
 
-        int i=0;
-        while(i<10){
-            System.out.print("hello");
-            System.out.println("world");
-
-            i=i+1;
         }
+
+        System.out.println("now iam out of the loop");
+
+        //int i=0;
+        // while(i<10){
+        //     System.out.print("hello");
+        //     System.out.println("world");
+
+        //     i=i+1;
+        // }
 
     }
 }
