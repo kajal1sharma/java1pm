79b328d7547f8c8efa03e10f5ffde151c08f582a
brainmentorspvtltd <kajal1231995@gmail.com>
2023-04-08 14:07:09 +0530

    pattern printing

--- Whileloop.java
+++ Whileloop.java
@@ -1,19 +1,59 @@
 public class Whileloop {
     public static void main(String[] args) {
         
-        int i =1;
-        while(true){
-            System.out.println(i*13+" " +i);
-            if(i>90){
-                break;
+        for(int i=1 ; i<6 ;i++){
+
+            //spaces
+            for( int s= 4 ;s >=i; s-- ){
+                System.out.print(" ");
             }
-            else{
-                i=i*13;
+            //star printing
+            for(int j=1 ;j <=i;j++){
+                System.out.print("*");
             }
+            System.out.println();
 
         }
 
-        System.out.println("now iam out of the loop");
+        // for(int i=1;i<=4;i++){
+            
+        //     for(int j=0; j<i;j++){
+        //         System.out.print("* ");
+        //     }
+        //     System.out.println("");
+        // }
+
+        // for(int i=1;i<=4;i++){
+            
+        //     for( int j =5 ;j>i;j=j-1){
+        //         System.out.print("* ");
+        //     }
+        //     System.out.println("");
+        // }
+
+
+        // for(int j=2; j<5; j=j+1){
+                
+        //        for(int i=0;i<5;i++){
+        //             System.out.print("*");
+        //         }
+        //         System.out.println("");
+        // }
+
+
+        // int i =1;
+        // while(true){
+        //     System.out.println(i*13+" " +i);
+        //     if(i>90){
+        //         break;
+        //     }
+        //     else{
+        //         i=i*13;
+        //     }
+
+        // }
+
+        // System.out.println("now iam out of the loop");
 
         //int i=0;
         // while(i<10){
