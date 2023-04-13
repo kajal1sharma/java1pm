dac9091f1bfa6eb8bf4908ba8a05a7b733bcac29
brainmentorspvtltd <kajal1231995@gmail.com>
2023-04-13 14:05:48 +0530

    function

--- ReturnFunc.java
+++ ReturnFunc.java
@@ -1,18 +1,22 @@
 import java.util.Scanner;
 public class ReturnFunc {
 
-    public static int product(){
+    public static int product(int num1, int num2){
+        int pro = num1*num2;
+        return pro;
+    }
+    public static void main(String[] args) {
         Scanner sc= new Scanner(System.in);
         System.out.println("enter first number");
         int a=sc.nextInt();
         System.out.println("Enter second number");
         int b =sc.nextInt();
+        int result = product(a,b);
+        System.out.println(result);
+         
+      
 
-        int pro = a*b;
-        return pro;
-    }
-    public static void main(String[] args) {
-          int result = product();
-          System.out.println(result);
+        
+
     }
 }
