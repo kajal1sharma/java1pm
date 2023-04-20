626aa10549c9c1dd802e4580b6fb99292432260c
brainmentorspvtltd <kajal1231995@gmail.com>
2023-04-20 14:05:38 +0530

    ..

--- StaticKeyword.java
+++ StaticKeyword.java
@@ -2,32 +2,56 @@
 
 class Student{
     private static String schoolName="DPS";
-    int totalmarks;
+    private int totalmarks;
    
-    public static void setSchoolName(String name){
-        schoolName=name;
+
+
+    public static void setTotalmarks(int marks){
+        schoolName="dbs";
     }
-    public static String getSchool(){
-        return schoolName;
-    }
+
     
-    public void setSchoolName1(String name){
-        schoolName=name;
-    }
-    public String getSchool1(){
-        return schoolName;
-    }
+    // public int getTotalmarks() {
+    //     return totalmarks;
+    // }
+    // public void setTotalmarks(int totalmarks) {
+    //     this.totalmarks = totalmarks;
+    // }
+
+
+
+
+    // public static void setSchoolName(String name){
+    //     schoolName=name;
+    // }
+    // public static String getSchool(){
+    //     return schoolName;
+    // }
+    
+    // public void setSchoolName1(String name){
+    //     schoolName=name;
+    // }
+    // public String getSchool1(){
+    //     return schoolName;
+    // }
 }
 
 public class StaticKeyword {
     
     public static void main(String[] args) {
-        Student.setSchoolName("DBPS");
-        String name= Student.getSchool();
+
+        Student.setTotalmarks();
+
+        Student s1= new Student();
+        s1.setTotalmarks(23);
+        s1.getTotalmarks();
+
+        // Student.setSchoolName("DBPS");
+        // String name= Student.getSchool();
 
 
-        Student s1=new Student();
-        s1.setSchoolName("DPS");
+        // Student s1=new Student();
+        // s1.setSchoolName("DPS");
         // System.out.println(Student.schoolName);
         // Student s1=new Student();
         // s1.schoolName="BPS";
