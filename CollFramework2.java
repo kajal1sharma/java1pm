f14e66f7ee590274c002a5b72cfe56101a949c92
brainmentorspvtltd <kajal1231995@gmail.com>
2023-05-06 14:05:29 +0530

    ..

--- CollFramework2.java
+++ CollFramework2.java
@@ -1,26 +1,73 @@
 import java.util.LinkedList;
+import java.util.Stack;
+import java.util.Vector;
+import java.util.HashSet;
 import java.util.Iterator;
 
 public class CollFramework2 {
     public static void main(String[] args) {
-        LinkedList<Integer> li = new LinkedList<>();
-        li.add(23);
-        li.add(54);
-        li.add(89);
-        LinkedList<Integer> li2 = new LinkedList<>();
-        li2.add(56);
-        li2.add(45);
-        li2.addAll(1, li);
-        li2.addFirst(12);
-        li2.addLast(14);
-        System.out.println(li2.contains(12));
-        li2.offer(23);
         
-        Iterator<Integer> i = li2.iterator();
 
-        while(i.hasNext()){
-            System.out.println(i.next());
-        }
+
+        HashSet<String> hs = new HashSet<>();
+        hs.add("first");
+        hs.add("first");
+
+        // System.out.println(hs);
+
+        HashSet<Integer> hs2 =new HashSet<>();
+        hs2.add(1);
+        hs2.add(1);
+        hs2.add(5);
+        hs2.add(4);
+        hs2.add(3);
+        hs2.add(5);
+        hs2.add(2);
+        System.out.println(hs2);
+        // Vector<String> vector = new Vector<>(12, 40);
+
+        // vector.add("first");
+        
+        //dynamic array
+
+        // Stack<String> st = new Stack<>();
+
+        // st.push("first");
+        // st.push("second");
+        // st.push("third");
+        // st.push("fourth");
+        // st.add(2, "two ans a half");
+       
+        // Iterator<String> i = st.iterator();
+
+        // while(i.hasNext()){
+        //     System.out.println(i.next());
+        // }
+        
+
+        //System.out.println("size of the stack = "+st.size());
+        // System.out.println(st.peek());
+        // st.pop();
+        // System.out.println(st.peek());
+
+        // LinkedList<Integer> li = new LinkedList<>();
+        // li.add(23);
+        // li.add(54);
+        // li.add(89);
+        // LinkedList<Integer> li2 = new LinkedList<>();
+        // li2.add(56);
+        // li2.add(45);
+        // li2.addAll(1, li);
+        // li2.addFirst(12);
+        // li2.addLast(14);
+        // System.out.println(li2.contains(12));
+        // li2.offer(23);
+        
+        // Iterator<Integer> i = li2.iterator();
+
+        // while(i.hasNext()){
+        //     System.out.println(i.next());
+        // }
 
 
 
